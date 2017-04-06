024f716b4e37543b3c5a0af9f07a7648430a38bf
Pant Signal <paulwebberuk@gmail.com>
2017-04-06 15:51:10 -0600

    Initial commit
    First part of AI script

---
+++ IHateMyLife.cs
@@ -0,0 +1,42 @@
+void IHateMyLife(){
+    while (Dead!=True && ProzacDose<=0){    
+        If (Sex=M)
+            GoodbyeColdColdWorld();
+        If (Sex=F){
+           If (age > 15 && age < 26){
+              JobAsStripper();
+           }
+           If (age > 26){
+              If(hasSugarDaddy){
+                while(!Divorced){
+                  trophyWife++;
+                  SoccerMom();
+                }
+                 Cougar();
+                 SpendAllSettlementMoney();
+                 Broke();
+                 GoodbyeColdColdWorld();     
+              }
+              If(HasPimp){
+                  for(int days = 0; days<30; days++){
+                      dailyBeatings++;
+                      SmokeCrack();
+                      InsertNewStd();
+                   }
+
+                 GoodbyeColdColdWorld();
+              }
+
+               GoodbyeColdColdWorld();
+
+
+            }
+            If (age = 26){
+                ThatTimeOfMonth=True;
+                EthnicBoyfriendDaddyHates=True;
+                Ovaries=sperm++;
+                HouseInForrestLawn(Welfare);
+            }
+        }
+    }
+}
